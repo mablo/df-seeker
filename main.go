0a24f98ac769d3b7bfa02109e42e373fee32f745
Michal Prochowski <michal@prochowski.pl>
2019-12-07 01:32:24 +0100

    refactor

--- main.go
+++ main.go
@@ -7,138 +7,79 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"math"
 	"os"
-	"sync"
 )
 
-var fileList = []string {}
-var hashes = map[string]string {}
+var Files = map[int64][]string {}
 
-const amount = 5
+type Duplicate struct {
+	Size int64
+	Sum string
+	Files []string
+}
+
+var Duplicates []Duplicate
 
 func main()  {
-	wordPtr := flag.String("path", ".", "a string")
+	wordPtr := flag.String("path", ".", "Directory path.")
 
 	flag.Parse()
 
-	parseDirectory(*wordPtr)
+	buildFilesMap(*wordPtr)
 
-	var wg sync.WaitGroup
-
-	maxOffset := int(math.Ceil(float64(len(fileList))/float64(amount)))
-	abc := make(chan map[string]string, maxOffset)
-	wg.Add(maxOffset)
-
-	for offset := 0; offset < maxOffset; offset++ {
-		x := offset*amount
-		y := (offset+1)*amount
-
-		//fmt.Printf("%d, %d, %d\n", x, y, maxOffset)
-
-		go func(files []string) {
-			defer wg.Done()
-			mappy := make(map[string]string)
-
-			for _, file := range files {
-				mappy[file] = getSize(file)
-			}
-			abc <- mappy
-		}(fileList[x:y])
-	}
-
-	go func(messages chan map[string]string) {
-		wg.Wait()
-		close(messages)
-	}(abc)
-
-	a := map[string][]string {}
-
-	for ret := range abc {
-		//fmt.Print(ret)
-		for x, y := range ret {
-			a[y] = append(a[y], x)
-		}
-	}
-
-	i := 0
-
-	for _, ret  := range a {
-		if len(ret) > 1 {
+	for size, list := range Files {
+		if len(list) > 1 {
 			hashes := map[string][]string {}
 
-			for _, file := range ret {
-				hash := getHash(file)
+			for _, file := range list {
+				hash := calculateHash(file)
 				hashes[hash] = append(hashes[hash], file)
 			}
 
-			for _, files := range hashes {
+			for hash, files := range hashes {
 				if len(files) > 1 {
-					fmt.Print("\n")
-					for _, file := range files {
-						fmt.Printf("%s\n", file)
-					}
-
-					fmt.Print("\n")
-					i++
+					Duplicates = append(Duplicates, Duplicate{
+						Size:  size,
+						Sum:   hash,
+						Files: files,
+					})
 				}
 			}
 		}
 	}
-
-	//fmt.Print(a)
-
-
-
-	//msg := <-abc
-	//fmt.Print(msg)
-	//wg.Wait()
-
-
-	//item := make(map[string]string)
-	//
-	//for ret := range abc {
-	//	for x, y := range ret {
-	//		item[x] = y
-	//	}
-	//}
-	//
-	//b, err := json.MarshalIndent(item, "", "  ")
-	//if err != nil {
-	//	fmt.Println("error:", err)
-	//}
-	//fmt.Print(string(b))
 }
 
-func parseDirectory(path string) {
-	files, _ := ioutil.ReadDir(path)
+func buildFilesMap(path string) {
+	items, _ := ioutil.ReadDir(path)
 
-	for _, file := range files {
-		if (file.IsDir()) {
-			parseDirectory(path + "/" + file.Name())
+	for _, item := range items {
+		if item.IsDir() {
+			buildFilesMap(path + "/" + item.Name())
 		} else {
-			fileList = append(fileList, path + "/" + file.Name())
+			Files[item.Size()] = append(Files[item.Size()], path + "/" + item.Name())
 		}
 	}
 }
 
-func getHash(file string) string {
+func calculateHash(file string) string {
 	fh, _ := os.Open(file)
 
 	h := sha256.New()
 	io.Copy(h, fh)
-
 	fh.Close()
 
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-func getSize(file string) string {
-	fi, err := os.Stat(file)
-
-	if err != nil {
-		return "error"
+func humanSize(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
 	}
-
-	return fmt.Sprintf("%8d", fi.Size())
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %ciB", float64(b)/float64(div), "KMGTPE"[exp])
 }
